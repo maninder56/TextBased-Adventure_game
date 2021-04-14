12c982117311e3e4f19b7b0eee3dffd8d5c68c9e
bodlah <bodlah@coventry.ac.uk>
2021-04-15 03:37:55 +0500

    added more functions

--- functions.h
+++ functions.h
@@ -9,6 +9,64 @@
 using namespace std;
 
 
+   //global vars
+int hit;        
+string name;
+int choice;
+int n_choice;  
+
+
+
+void fight(int getp_health, int getm_health, int getp_def, int getp_hitp, string getd_options){                
+    if (getp_health<=0){
+    cout<< "R.I.P "<<name<<" you died fighting a monster :'( "<<endl;
+    exit(0);
+    }  
+
+    cout<<"Monster"<<endl;
+    cout<<"hp: "<<getm_health<<"   "<<"attack power: <HIDDEN>"<<endl<<endl;
+    Sleep(3000);
+
+    cout<<name<<endl;
+    cout<<"hp: "<<getp_health<<"   "<<"armour: "<<getp_def<<"   "<<"attack power: "<<getp_hitp<<endl<<endl;
+    Sleep(4000);
+
+    cout<<"from the following, choose one: "<<endl;
+    cout<<getd_options<<endl;
+    cin>>choice;
+    cout<<endl;
+}
+
+void final_fight(int getp_health, int getm_health, int getp_def, int getp_hitp,int getm_armour, string getd_options){                
+    if (getp_health<=0){
+    cout<< "R.I.P "<<name<<" you died fighting a monster :'( "<<endl;
+    exit(0);
+    }  
+
+    cout<<"Monster"<<endl;
+    cout<<"hp: "<<getm_health<<"   "<<"attack power: <HIDDEN>"<<"armour: "<<getm_armour<<endl<<endl;
+    Sleep(3000);
+
+    cout<<name<<endl;
+    cout<<"hp: "<<getp_health<<"   "<<"armour: "<<getp_def<<"   "<<"attack power: "<<getp_hitp<<endl<<endl;
+    Sleep(4000);
+
+    cout<<"from the following, choose one: "<<endl;
+    cout<<getd_options<<endl;
+    cin>>choice;
+    cout<<endl;
+}
+        
+void info(int getp_health, int getp_def, int getp_hitp, int getp_coins){
+    Sleep(2000);
+    cout<<"Please take a moment to look at the following "<<endl;
+    Sleep(1500);
+    cout<<"Your HP is: "<<getp_health<<endl;
+    cout<<"Your armour is: "<<getp_def<<endl;
+    cout<<"Your attack power is: "<<getp_hitp<<endl;
+    cout<<"You have "<<getp_coins<<" coins"<<endl<<endl;
+    Sleep(6000);
+}
 
 
 void disp(string get_disp, int slp){
